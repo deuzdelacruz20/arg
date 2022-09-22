0bd5acf5d8401b25346a8fbc5ce0b8718e03b310
deuzdelacruz20 <ardeuzdelacruz20@gmail.com>
2022-09-22 20:12:09 +0800

    added database

--- schedules.php
+++ schedules.php
@@ -10,6 +10,8 @@ include 'connect.php';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ARG AutoSign Shop</title>
     <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </head>
 <body>
     <h1>Schedules</h1>
@@ -24,6 +26,7 @@ include 'connect.php';
             <th>Services</th>
             <th>Operations</th>
         </tr>
+        
         <?php
         // $conn = new mysqli('localhost','root','','arg');
         // if($conn->connect_error){
@@ -54,7 +57,13 @@ include 'connect.php';
                     <td>"
                         .$row["services"].
                     "</td>
-                </tr>";
+                    <td>"
+                    ?>
+                    <a href="#"><button type="button" class="btn btn-primary">Update</button></a>
+                    <a href="#"><button type="button" class="btn btn-danger">Delete</button></a>
+                    </td>
+                </tr>
+            <?php
             }
             echo "</table>";
         }
