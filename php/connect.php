25a52b8d8bf194f325e4ef440bbc61b9d4a4ae23
deuzdelacruz20 <ardeuzdelacruz20@gmail.com>
2022-09-27 19:20:29 +0800

    code cleanup

--- php/connect.php
+++ php/connect.php
@@ -1,27 +1,4 @@
 <?php
-    // START 
-
-    // $fullName = $_POST['fullName'];
-    // $phoneNumber = $_POST['phoneNumber'];
-    // $time = $_POST['time'];
-    // $services = $_POST['services'];
-    
-    // //connection
-    // $conn = new mysqli('localhost','root','','arg');
-    // if($conn->connect_error){
-    //     die('Connection Failed: ' .$conn->connect_error);
-    // }
-    // else{
-    //     $stmt = $conn->prepare("insert into customer_request(fullName, phoneNumber, time, services) values(?,?,?,?)");
-    // }
-    // $stmt->bind_param("siss", $fullName, $phoneNumber, $time, $services);
-    // $stmt->execute();
-    // echo "Appointment Schedule Request submitted successfully";
-    // $stmt->close();
-    // $conn->close();
-
-    // END
-
     // CONNECTION
     session_start();
     $conn=new mysqli('localhost','root','','arg');
