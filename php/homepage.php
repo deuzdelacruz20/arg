987c4451f2061539b4c59b952eb71d12bebeb634
deuzdelacruz20 <ardeuzdelacruz20@gmail.com>
2022-09-29 23:01:13 +0800

    update database

--- php/homepage.php
+++ php/homepage.php
@@ -13,6 +13,12 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
 
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto|Varela+Round">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"> -->
   </head>
 <body>
   <h1>Homepage haha</h1>
--- php/schedules.php
+++ php/schedules.php
@@ -1,6 +1,6 @@
 <?php 
 include '../include/navigation.php';
-include 'connect.php';
+include 'backend/database.php';
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -9,65 +9,191 @@ include 'connect.php';
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ARG AutoSign Shop</title>
-    <link rel="stylesheet" href="../css/schedules.css">
+    <link rel="stylesheet" href="css/schedules.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+	
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto|Varela+Round">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<script src="ajax/ajax.js"></script>
+
 </head>
 <body>
-    <h1>Schedules</h1>
-    <!-- DISPLAY THE DATA ON DATABASE -->
-    <table class="table table-hover table-dark">
-        <!-- table -->
-        <tr>
-            <th>ID</th>
-            <th>Full Name</th>
-            <th>Phone Number</th>
-            <th>Time</th>
-            <th>Services</th>
-            <th>Operations</th>
-        </tr>
-        
-        <?php
-        // READ
-        $sql = "SELECT id, fullName, phoneNumber, time, services from customer_request";
-        $result = $conn->query($sql);
-        
-        if($result-> num_rows > 0){
-            //output
-            while($row = $result->fetch_assoc()){
-                echo 
-                "<tr>
-                    <td>"
-                        .$row["id"].
-                    "</td>
-                    <td>"
-                        .$row["fullName"].
-                    "</td>
-                    <td>0"
-                        .$row["phoneNumber"].
-                    "</td>
-                    <td>"
-                        .$row["time"].
-                    "</td>
-                    <td>"
-                        .$row["services"].
-                    "</td>
-                    <td>"
-                    ?>
-                    <a href="form2.php?updateid=<?php echo $row['id']?>"><button type="button" class="btn btn-primary">Update</button></a>
-                    <a href="process.php?delete=<?php echo $row['id']?>"><button type="button" class="btn btn-danger">Delete</button></a>
+<div class="container">
+	<p id="success"></p>
+        <div class="table-wrapper" style="background-color: white; border-radius: 10px;">
+            <div class="table-title" style="border-top-left-radius: 10px; border-top-right-radius: 10px;">
+                <div class="row">
+                    <div class="col-sm-6">
+						<h2><b>Schedules</b></h2>
+					</div>
+					<div class="col-sm-6">
+						<a href="#addEmployeeModal" class="btn btn-success" data-toggle="modal"><i class="material-icons">&#xE147;</i> <span>Add New User</span></a>
+						<a href="JavaScript:void(0);" class="btn btn-danger" id="delete_multiple"><i class="material-icons">&#xE15C;</i> <span>Delete</span></a>						
+					</div>
+                </div>
+            </div>
+            <table class="table table-striped table-hover">
+                <thead>
+                    <tr>
+						<th>
+							<span class="custom-checkbox">
+								<input type="checkbox" id="selectAll">
+								<label for="selectAll"></label>
+							</span>
+						</th>
+						<th>ID</th>
+                        <th>Full Name</th>
+                        <th>Phone Number</th>
+						<th>Time</th>
+                        <th>Services</th>
+                        <th>Operations</th>
+                    </tr>
+                </thead>
+				<tbody>
+				
+				<!-- DISPLAY DATA IN A TABLE -->
+				<?php
+				$result = mysqli_query($conn,"SELECT * FROM customer_request");
+					$i=1;
+					while($row = mysqli_fetch_array($result)) {
+				?>
+				<tr id="<?php echo $row["id"]; ?>">
+				<td>
+							<span class="custom-checkbox">
+								<input type="checkbox" class="user_checkbox" data-user-id="<?php echo $row["id"]; ?>">
+								<label for="checkbox2"></label>
+							</span>
+						</td>
+					<td><?php echo $row["id"]; ?></td>
+					<td><?php echo $row["fullName"]; ?></td>
+					<td>0<?php echo $row["phoneNumber"]; ?></td>
+					<td><?php echo $row["time"]; ?></td>
+					<td><?php echo $row["services"]; ?></td>
+					<td>
+						<a href="#editEmployeeModal" class="edit" data-toggle="modal">
+							<i class="material-icons update" data-toggle="tooltip" 
+							data-id="<?php echo $row["id"]; ?>"
+							data-fullName="<?php echo $row["fullName"]; ?>"
+							data-phoneNumber="0<?php echo $row["phoneNumber"]; ?>"
+							data-time="<?php echo $row["time"]; ?>"
+							data-services="<?php echo $row["services"]; ?>"
+							title="Edit">&#xE254;</i>
+						</a>
+						<a href="#deleteEmployeeModal" class="delete" data-id="<?php echo $row["id"]; ?>" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" 
+						 title="Delete">&#xE872;</i></a>
                     </td>
-                </tr>
-            <?php
-            }
-            echo "</table>";
-        }
-        // else{
-        //     echo "0 result";
-        // }
-        $conn->close();
-        ?>
-    </table>
-    <button onclick="history.back()">Go Back</button>
+				</tr>
+				<?php
+				$i++;
+				}
+				?>
+				</tbody>
+			</table>
+			
+        </div>
+    </div>
+
+		<!-- Add Modal HTML -->
+		<div id="addEmployeeModal" class="modal fade">
+		<div class="modal-dialog">
+			<div class="modal-content">
+				<form id="user_form">
+					<div class="modal-header">						
+						<h4 class="modal-title">Add customer</h4>
+						<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+					</div>
+					<div class="modal-body">					
+						<div class="form-group">
+							<label>Full Name</label>
+							<input type="text" id="fullName" name="fullName" class="form-control" required>
+						</div>
+						<div class="form-group">
+							<label>Phone Number</label>
+							<input type="text" id="phoneNumber" name="phoneNumber" class="form-control" required>
+						</div>
+						<div class="form-group">
+							<label>Time</label>
+							<input type="phone" id="time" name="time" class="form-control" required>
+						</div>
+						<div class="form-group">
+							<label>Services</label>
+							<input type="text" id="services" name="services" class="form-control" required>
+						</div>					
+					</div>
+					<div class="modal-footer">
+					    <input type="hidden" value="1" name="type">
+						<input type="button" class="btn btn-default" data-dismiss="modal" value="Cancel">
+						<button type="button" class="btn btn-success" id="btn-add">Add</button>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+
+	<!-- Edit Modal HTML -->
+	<div id="editEmployeeModal" class="modal fade">
+			<div class="modal-dialog">
+				<div class="modal-content">
+					<form id="update_form">
+						<div class="modal-header">						
+							<h4 class="modal-title">Edit User</h4>
+							<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+						</div>
+						<div class="modal-body">
+							<input type="hidden" id="id_u" name="id" class="form-control" required>					
+							<div class="form-group">
+								<label>Name</label>
+								<input type="text" id="fullName_u" name="fullName" class="form-control" required>
+							</div>
+							<div class="form-group">
+								<label>Phone Number</label>
+								<input type="phone" id="phoneNumber_u" name="phoneNumber" class="form-control" required>
+							</div>
+							<div class="form-group">
+								<label>Services</label>
+								<input type="text" id="services_u" name="services" class="form-control" required>
+							</div>
+							<div class="form-group">
+								<label>Time</label>
+								<input type="text" id="time_u" name="time" class="form-control" required>
+							</div>				
+						</div>
+						<div class="modal-footer">
+						<input type="hidden" value="2" name="type">
+							<input type="button" class="btn btn-default" data-dismiss="modal" value="Cancel">
+							<button type="button" class="btn btn-info" id="update">Update</button>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+
+		<!-- Delete Modal HTML -->
+		<div id="deleteEmployeeModal" class="modal fade">
+			<div class="modal-dialog">
+				<div class="modal-content">
+					<form>
+						<div class="modal-header">						
+							<h4 class="modal-title">Delete</h4>
+							<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+						</div>
+						<div class="modal-body">
+							<input type="hidden" id="id_d" name="id" class="form-control">					
+							<p>Are you sure you want to delete these Records?</p>
+							<p class="text-warning"><small>This action cannot be undone.</small></p>
+						</div>
+						<div class="modal-footer">
+							<input type="button" class="btn btn-default" data-dismiss="modal" value="Cancel">
+							<button type="button" class="btn btn-danger" id="delete">Delete</button>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
 </body>
 </html>
