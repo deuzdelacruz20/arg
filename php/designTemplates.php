e91a748b1727d03befa0863037876e31dcbc6e09
deuzdelacruz20 <ardeuzdelacruz20@gmail.com>
2022-10-13 21:29:18 +0800

    css updates

--- php/designTemplates.php
+++ php/designTemplates.php
@@ -17,7 +17,7 @@
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 	<script src="ajax/ajax.js"></script>
-    <link rel="stylesheet" href="css/schedules.css">
+    <link rel="stylesheet" href="../css/schedules.css">
 </head>
 <body>
     <h1>Design Templates</h1>
--- php/homepage.php
+++ php/homepage.php
@@ -19,7 +19,7 @@ include 'backend/database.php';
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 	<script src="ajax/ajax.js"></script>
-	<link rel="stylesheet" href="css/schedules.css">
+	<link rel="stylesheet" href="../css/schedules.css">
 
 </head>
 <body>
