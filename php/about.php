181a34b6960a1ae52ffbf6727a89e8381eaf5a5c
leighayuma <106870375+leighayuma@users.noreply.github.com>
2022-10-20 08:28:30 +0800

    modified about

--- php/about.php
+++ php/about.php
@@ -21,6 +21,49 @@
     
 </head>
 <body>
-    <h1>About</h1>
+<section id="ABOUT">
+     <div class="about-1">
+         <h1> ABOUT </h1>
+         
+     </div>
+     <div id="about-2">
+     <div class="content-box-lg">
+         <div class="container">
+             <div class="row">
+                 <div class="col-md-4">
+                    <div class="about-item text-center">
+                     <i class="fa fa-book"></i>
+                     <h3>MISSION</h3>
+                     <hr>
+                     <p> We deliver creative Print & Marketing Solutions that make our clients successful. We keep our promises, provide creative solutions, and achieve our client’s goals. </p>
+                     </div>
+                 </div>
+                 <div class="col-md-4">
+                    <div class="about-item text-center">
+                     <i class="fa fa-globe"></i>
+                     <h3>VISSION</h3>
+                     <hr>
+                     <p> "To be the world's most premier printing solution company delivering premium quality product to our customers." </p>
+                     </div>
+                     <div class="footer-area">
+          <div class="callus">
+            <h3>CALL US</h3>
+            <p>091234567890 </p>   
+          </div>
+          <div class="visitus">
+            <h3>VISIT US</h3>
+            <p>Barangay Isabang Tayabas City </p>
+           
+          </div>
+</div>
+                 
+      </div>   
+ </section>
+    
+<!--our footer -->
+ 
+<footer class="text-center">
+
+</footer>
 </body>
 </html>
