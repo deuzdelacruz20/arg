41586f9c0c3742b1c694d5454abf1a3b7bd52246
leighayuma <106870375+leighayuma@users.noreply.github.com>
2022-10-27 14:36:00 +0800

    fixed about page

--- php/about.php
+++ php/about.php
@@ -3,71 +3,74 @@
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ARG AutoSign Shop</title>
-    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>ARG AutoSign Shop</title>
+  <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
      -->
-    <link rel="stylesheet" href="../css/schedules.css">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto|Varela+Round">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <script src="../ajax/ajax.js"></script>
-    
+  <link rel="stylesheet" href="../css/schedules.css">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto|Varela+Round">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="../ajax/ajax.js"></script>
+
 </head>
+
 <body>
-<section id="ABOUT">
-     <div class="about-1">
-         <h1> ABOUT </h1>
-         
-     </div>
-     <div id="about-2">
-     <div class="content-box-lg">
-         <div class="container">
-             <div class="row">
-                 <div class="col-md-4" >
-                    <div class="about-item text-center"style="background:white; border-radius:5px;">
-                     <i class="fa fa-book"></i>
-                     <h3>MISSION</h3>
-                     <hr>
-                     <p> We deliver creative Print & Marketing Solutions that make our clients successful. We keep our promises, provide creative solutions, and achieve our client’s goals. </p>
-                     </div>
-                 </div>
-                 <div class="col-md-4" style="background:white; border-radius:5px;">
-                    <div class="about-item text-center">
-                     <i class="fa fa-globe"></i>
-                     <h3>VISSION</h3>
-                     <hr>
-                     <p> "To be the world's most premier printing solution company delivering premium quality product to our customers." </p>
-                     </div>
-                     
-</div>
-<div class="footer-area">
-          <div class="callus">
-            <h3>CALL US</h3>
-            <p>091234567890 </p>   
-          </div>
+  <section id="ABOUT">
+    <div class="about-1">
+      <h1> ABOUT </h1>
 
-          <div class="visitus">
-            <h3>VISIT US</h3>
-            <p>Barangay Isabang Tayabas City </p>
-           
-          </div>
-</div>
-        </div>         
-      </div>   
- </section>
-    
-<!--our footer -->
- 
-<footer class="text-center">
+    </div>
+    <div id="about-2">
+      <div class="content-box-lg">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-4">
+              <div class="about-item text-center" style="background:white; border-radius:5px;">
+                <i class="fa fa-book"></i>
+                <h3>MISSION</h3>
+                <hr>
+                <p> We deliver creative Print & Marketing Solutions that make our clients successful. We keep our promises, provide creative solutions, and achieve our client’s goals. </p>
+              </div>
+            </div>
+            <div class="col-md-4" style="background:white; border-radius:5px;">
+              <div class="about-item text-center">
+                <i class="fa fa-globe"></i>
+                <h3>VISION</h3>
+                <hr>
+                <p> "To be the world's most premier printing solution company delivering premium quality product to our customers." </p>
+              </div>
 
-</footer>
+            </div>
+            <div class="footer-area">
+              <div class="callus">
+                <h3>CALL US</h3>
+                <p>091234567890 </p>
+              </div>
+
+              <div class="visitus">
+                <h3>VISIT US</h3>
+                <p>Barangay Isabang Tayabas City </p>
+
+              </div>
+            </div>
+          </div>
+        </div>
+  </section>
+
+  <!--our footer -->
+
+  <footer class="text-center">
+
+  </footer>
 </body>
+
 </html>
