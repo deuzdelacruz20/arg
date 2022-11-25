2332d34abb88a7c44de8db329013a3222c35422c
deuzdelacruz20 <ardeuzdelacruz20@gmail.com>
2022-11-25 14:37:30 +0800

    fixed navigation

--- include/navigation.php
+++ include/navigation.php
@@ -1,12 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>ARG AutoSign Shop</title>
-  </head>
-
   <!-- <link rel="stylesheet" type="text/css" href="styles.css" /> -->
   <!-- <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto|Varela+Round">
   <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
@@ -14,7 +13,9 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
   <!-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"> -->
+</head>
 
+<body>
 
 
   <nav class="navbar navbar-default">
@@ -47,7 +48,7 @@
         <ul class="nav navbar-nav navbar-right">
           <li><a href="logout.php">Logout</a></li>
         </ul>
-    </div>
+      </div>
   </nav>
 
   <script>
@@ -58,5 +59,6 @@
       $(this).addClass('active');
     });
   </script>
-  </body>
+</body>
+
 </html>
