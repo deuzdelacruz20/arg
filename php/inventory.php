826cd9fba357436e4521d60de4f4a54c8a814a06
leighayuma <106870375+leighayuma@users.noreply.github.com>
2022-12-13 23:28:17 +0800

    added input field for selling price

--- php/inventory.php
+++ php/inventory.php
@@ -369,8 +369,12 @@ include '../backend/database.php';
 							<input type="text" id="itemName" name="itemName" class="form-control" placeholder="Enter the Item Name" required>
 						</div>
 						<div class="form-group">
-							<label>PRICE</label>
-							<input type="number" id="itemPrice" name="itemPrice" class="form-control" placeholder="Enter the Item Price" required>
+							<label>PRICE BOUGHT</label>
+							<input type="number" id="itemPrice" name="itemPrice" class="form-control" placeholder="Enter the Item Price bought" required>
+						</div>
+						<div class="form-group">
+							<label>SELLING PRICE</label>
+							<input type="number" id="itemPriceSell" name="itemPriceSell" class="form-control" placeholder="Enter the Item Price to sell" required>
 						</div>
 						<div class="form-group">
 							<label>STOCKS</label>
