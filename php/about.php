7e18e5c585876bb5b39033fa399cf9a10d210065
deuzdelacruz20 <ardeuzdelacruz20@gmail.com>
2022-12-13 16:28:47 +0800

    about

--- php/about.php
+++ php/about.php
@@ -5,32 +5,32 @@
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>ARG AutoSign Shop</title>
-  <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>ARG AutoSign Shop</title>
+	<!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
      -->
-  <link rel="stylesheet" href="../css/about.css">
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto|Varela+Round">
-  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <script src="../ajax/ajax.js"></script>
-  <style>
-    body {
-      background: linear-gradient(120deg, #71b7e6, #9b59b6);
-      background-attachment: fixed;
-    }
-  </style>
+	<link rel="stylesheet" href="../css/about.css">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto|Varela+Round">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<script src="../ajax/ajax.js"></script>
+	<style>
+		body {
+			background: linear-gradient(120deg, #71b7e6, #9b59b6);
+			background-attachment: fixed;
+		}
+	</style>
 
 </head>
 
 <body>
-<div class="section">
+	<!-- <div class="section">
 		<div class="container">
 			<div class="content-section">
 				<div class="title">
@@ -39,8 +39,8 @@
 				<div class="content">
 					<h3>Mission</h3>
 					<p>We deliver creative Print & Marketing Solutions that make our clients successful. We keep our promises, provide creative solutions, and achieve our client’s goals.</p>
-         			<h3>Vission</h3>
-          			<p>To be the world's most premier printing solution company delivering premium quality product to our customers.</p>
+					<h3>Vission</h3>
+					<p>To be the world's most premier printing solution company delivering premium quality product to our customers.</p>
 					<div class="button">
 						<a href="">Read More</a>
 					</div>
@@ -54,13 +54,23 @@
 			</div>
 		</div>
 	</div>
-<footer class="text-center">
+	<footer class="text-center">
 		<h4><i class="fa fa-phone"></i></i> Contact</h4>
 		<h4> 099999999</h4>
 		<h4><i class="fa fa-map-marker"></i> Location</h4>
 		<h4>Barangay Isabang Tayabas City.</h4>
 		<p>&copy; 2022, ARG Authosign Shop.</p>
-	</footer>
+	</footer> -->
+	<div class="container" style="background-color: white; text-align:center;">
+		<div class="row">
+			<div class="col-md-6">
+				column 1
+			</div>
+			<div class="col-md-6">
+				<img src="../image/arg logo.png">
+			</div>
+		</div>
+	</div>
 </body>
 
 </html>
