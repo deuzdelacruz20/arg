57468d55095633d67ee4fd958ad1b56ab6f474ec
leighayuma <106870375+leighayuma@users.noreply.github.com>
2022-12-13 23:10:03 +0800

    about page UI updates

--- php/about.php
+++ php/about.php
@@ -3,74 +3,153 @@
 
 <!DOCTYPE html>
 <html lang="en">
-
-<head>
-	<meta charset="UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>ARG AutoSign Shop</title>
-	<!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>ARG AutoSign Shop</title>
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
      -->
-	<link rel="stylesheet" href="../css/about.css">
-	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto|Varela+Round">
-	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-	<script src="../ajax/ajax.js"></script>
-	<style>
-		body {
-			background: linear-gradient(120deg, #71b7e6, #9b59b6);
-			background-attachment: fixed;
-		}
-	</style>
+    <link rel="stylesheet" href="../css/about.css" />
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css?family=Roboto|Varela+Round"
+    />
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/icon?family=Material+Icons"
+    />
+    <link
+      rel="stylesheet"
+      href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
+    />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link
+      rel="stylesheet"
+      href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+    />
+    <script src="../ajax/ajax.js"></script>
+    <style>
+      body {
+        background: linear-gradient(120deg, #71b7e6, #9b59b6);
+        background-attachment: fixed;
+      }
+    </style>
+  </head>
 
-</head>
-
-<body>
-	<!-- <div class="section">
-		<div class="container">
-			<div class="content-section">
-				<div class="title">
-					<h1>About Us</h1>
-				</div>
-				<div class="content">
-					<h3>Mission</h3>
-					<p>We deliver creative Print & Marketing Solutions that make our clients successful. We keep our promises, provide creative solutions, and achieve our client’s goals.</p>
-					<h3>Vission</h3>
-					<p>To be the world's most premier printing solution company delivering premium quality product to our customers.</p>
-					<div class="button">
-						<a href="">Read More</a>
-					</div>
-				</div>
-				<div class="social">
-					<a href="https://www.facebook.com/ARGAutosignShop" target="_blank"><img src="../image/Fbicon.png" width="40" height="40/"></a>
-				</div>
-			</div>
-			<div class="image-section">
-				<img src="../image/arg logo.png">
-			</div>
-		</div>
-	</div>
-	<footer class="text-center">
-		<h4><i class="fa fa-phone"></i></i> Contact</h4>
-		<h4> 099999999</h4>
-		<h4><i class="fa fa-map-marker"></i> Location</h4>
-		<h4>Barangay Isabang Tayabas City.</h4>
-		<p>&copy; 2022, ARG Authosign Shop.</p>
-	</footer> -->
-	<div class="container" style="background-color: white; text-align:center;">
-		<div class="row">
-			<div class="col-md-6">
-				column 1
-			</div>
-			<div class="col-md-6">
-				<img src="../image/arg logo.png">
-			</div>
-		</div>
-	</div>
-</body>
-
+  <body>
+    <div class="container" style="text-align: center; width: 100%; margin-top: 20px;">
+      <div class="row" style="margin-top: 35px;">
+        <div class="col-md-6">
+          <div class="content" style="text-align: left; font-size: 25px;">
+            <h1 style="font-size: 50px; font-weight: bold;">About us</h1>
+            <h1 style="font-weight: bold;">Mission</h1>
+            <p>
+              We deliver creative Print & Marketing Solutions that make our
+              clients successful. We keep our promises, provide creative
+              solutions, and achieve our client’s goals.
+            </p>
+            <h1 style="font-weight: bold;">Vision</h1>
+            <p>
+              To be the world's most premier printing solution company
+              delivering premium quality product to our customers.
+            </p>
+          </div>
+        </div>
+        <div class="col-md-6">
+            <img src="../image/arg logo.png" width="600" height="600" style="padding: 30px; align-items: center; margin-bottom: 80px;"/>
+        </div>
+      </div>
+    </div>
+    <div class="container">
+      <h1>Our Services</h1>
+      <div class="row">
+        <div class="col-md-4">
+          <div class="thumbnail">
+            <a href="../image/car5png" target="_blank">
+              <img src="../image/car5.png" alt="Lights" style="width:100%">
+              <div class="caption">
+                <p>Full Wrap</p>
+              </div>
+            </a>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="thumbnail">
+            <a href="../image/car2.png" target="_blank">
+              <img src="../image/car2.png" alt="Nature" style="width:100%">
+              <div class="caption">
+                <p>Hood Wrap</p>
+              </div>
+            </a>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="thumbnail">
+            <a href="../image/car6.png" target="_blank">
+              <img src="../image/car6.png" alt="Fjords" style="width:100%">
+              <div class="caption">
+                <p>Headlight Film</p>
+              </div>
+            </a>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-6">
+            <div class="thumbnail">
+              <a href="../image/car7.png" target="_blank">
+                <img src="../image/car7.png" alt="Lights"style="width:100%" >
+                <div class="caption">
+                  <p>Customize Plate</p>
+                </div>
+              </a>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="thumbnail">
+              <a href="../image/car9.png" target="_blank">
+                <img src="../image/car9.png" alt="Nature" style="width:100%">
+                <div class="caption">
+                  <p>Signage</p>
+                </div>
+              </a>
+            </div>
+          </div>
+      </div>
+    </div>
+    </div>
+    <div class="container" style="background-color: white; text-align: center; width: 500px;">
+      <h1>Time Open</h1>
+          <h3>BUSINESS DAYS</h3>
+          <p>We are open from Mondays to Saturday
+            7:00AM to 6:00PM
+          </p>
+          <h3>BUSINESS HOURS</h3>
+          <div class="row">
+          <div class="col-md-6">
+            <h4>Morning</h4>
+              <p>7:00 AM - 8:00 AM</p>
+              <p>8:00 AM - 9:00 AM</p>
+              <p>9:00 AM - 10:00 AM</p>
+              <p>11:00 AM - 12:00 AM</p>
+    </div>
+    <div class="col-md-6">
+      <h4>Afternoon</h4>
+        <p>12:00 PM - 1:00 PM</p>
+        <p>1:00 PM - 2:00 PM</p>
+        <p>3:00 PM - 4:00 PM</p>
+        <p>4:00 PM - 5:00 PM</p>
+        <p>5:00 PM - 6:00 PM</p>
+</div>
+</div>
+</div>
+    <footer class="text-center">
+      <h4><i class="fa fa-phone"></i></i> Contact</h4>
+      <h4> 099999999</h4>
+      <h4><i class="fa fa-map-marker"></i> Location</h4>
+      <h4>Barangay Isabang Tayabas City.</h4>
+      <p>&copy; 2022, ARG Authosign Shop.</p>
+  </body>
 </html>
