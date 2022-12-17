dad383b8cc9d624b0888b5ffec1a11853c09c35b
deuzdelacruz20 <ardeuzdelacruz20@gmail.com>
2022-12-18 02:28:39 +0800

    added no empty data feature in schedule request form

--- backend/save.php
+++ backend/save.php
@@ -1,6 +1,8 @@
 <?php
 include 'database.php';
 
+$error = NULL;
+
 if (count($_POST) > 0) {
 	if ($_POST['type'] == 1) {
 		$firstName = $_POST['firstName'];
@@ -9,14 +11,35 @@ if (count($_POST) > 0) {
 		$date = $_POST['date'];
 		$services = $_POST['inputServices'];
 		$time = $_POST['inputTime'];
-		$sql = "INSERT INTO `customer_request`(`firstName`,`lastName`,`phoneNumber`,`date`,`services`,`time`, `timestamp`) 
+
+		if (empty($firstName)) {
+			$error = 'Enter Your First Name!';
+		} 
+		else if (empty($lastName)) {
+			$error = 'Enter Your Last Name!';
+		} 
+		else if (empty($phoneNumber)) {
+			$error = 'Enter Your Phone Number!';
+		} 
+		else if (empty($date)) {
+			$error = 'Enter a Valid Date!';
+		} 
+		else if (empty($_POST['inputServices'])) {
+			$error = 'Choose a Service!';
+		} 
+		else if (empty($_POST['inputTime'])) {
+			$error = 'Choose a Timeslot!';
+		} 
+		else {
+			$sql = "INSERT INTO `customer_request`(`firstName`,`lastName`,`phoneNumber`,`date`,`services`,`time`, `timestamp`) 
 		VALUES ('$firstName','$lastName','$phoneNumber','$date','$services','$time',current_timestamp())";
-		if (mysqli_query($conn, $sql)) {
-			echo json_encode(array("statusCode" => 200));
-		} else {
-			echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+			if (mysqli_query($conn, $sql)) {
+				echo json_encode(array("statusCode" => 200));
+			} else {
+				echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+			}
+			mysqli_close($conn);
 		}
-		mysqli_close($conn);
 	}
 }
 if (count($_POST) > 0) {
