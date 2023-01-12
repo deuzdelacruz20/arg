5554bd28be483d13643e484542f770075660124d
deuzdelacruz20 <ardeuzdelacruz20@gmail.com>
2023-01-12 11:08:25 +0800

    database update

--- db-script/arg.sql
+++ db-script/arg.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Jan 11, 2023 at 03:16 AM
+-- Generation Time: Jan 12, 2023 at 04:06 AM
 -- Server version: 10.4.25-MariaDB
 -- PHP Version: 8.1.10
 
@@ -60,35 +60,17 @@ CREATE TABLE `customer_request` (
   `selectedPrice` int(11) NOT NULL,
   `selectedStocks` int(11) NOT NULL,
   `selectedCategory` varchar(255) NOT NULL,
-  `totalAmount` int(11) NOT NULL DEFAULT 0
+  `totalAmount` int(11) NOT NULL DEFAULT 0,
+  `refNumber` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `customer_request`
 --
 
-INSERT INTO `customer_request` (`id`, `firstName`, `lastName`, `phoneNumber`, `date`, `services`, `time`, `timestamp`, `user_status`, `earnings`, `selectedItem`, `selectedPrice`, `selectedStocks`, `selectedCategory`, `totalAmount`) VALUES
-(280, 'Deuz', 'De la Cruz', '09124567893', '2023-01-10', 'Full Wrap', '7:00 AM - 8:00 AM', '2022-12-28 08:18:38', 'Done', 200, '	No Selected item', 0, 0, '', 0),
-(281, 'Vince', 'Mendoza', '09456789123', '2023-01-10', 'Hood Wrap', '8:00 AM - 9:00 AM', '2022-12-28 08:23:22', 'Pending', 200, '	No Selected item', 0, 0, '', 0),
-(283, 'Ronel', 'Tagala', '09454654564', '2023-01-10', 'Hood Wrap', '7:00 AM - 8:00 AM', '2022-12-28 09:53:20', 'Pending', 200, '	No Selected item', 0, 0, '', 0),
-(285, 'Leigh', 'Ayuma', '09456789123', '2023-01-10', 'Hood Wrap', '10:00 AM - 11:00 AM', '2022-12-28 09:56:12', 'Pending', 200, '	No Selected item', 0, 0, '', 0),
-(286, 'Aldous', 'Turgo', '09789456456', '2023-01-10', 'Hood Wrap', '9:00 AM - 10:00 AM', '2022-12-28 11:10:29', 'Pending', 200, '	No Selected item', 0, 0, '', 0),
-(287, 'Ronald', 'Parcarey', '09456789123', '2023-01-10', 'Customized Plate', '1:00 PM - 2:00 PM', '2023-01-05 17:21:40', 'Pending', 200, '	No Selected item', 0, 0, '', 0),
-(295, 'Leigh ', 'Turgo', '09454645464', '2023-01-09', 'HeadLight Film', '7:00 AM - 8:00 AM', '2023-01-08 11:51:28', 'Pending', 200, '	No Selected item', 0, 0, '', 0),
-(296, 'Leigh ', 'Turgo', '09454645464', '2023-01-09', 'HeadLight Film', '7:00 AM - 8:00 AM', '2023-01-08 11:51:48', 'Pending', 200, '	No Selected item', 0, 0, '', 0),
-(297, 'Terence', 'Romeo', '09456456456', '2023-01-11', '2,000 - Hood Wrap', '7:00 AM - 8:00 AM', '2023-01-10 06:37:42', 'Done', 200, 'ARROW', 20, 10, 'Stickers', 0),
-(298, 'Jordan', 'Clarkson', '09123121231', '2023-01-11', 'Hood Wrap', '8:00 AM - 9:00 AM', '2023-01-10 07:03:35', 'Done', 200, '', 0, 0, '', 0),
-(299, 'Stephen', 'Curry', '12312312123', '2023-01-11', 'Hood Wrap', '9:00 AM - 10:00 AM', '2023-01-10 08:34:18', 'Done', 200, 'ARROW', 20, 10, 'Stickers', 0),
-(301, 'Michael', 'Jordan', '11231231313', '2023-01-11', '2,000 - Hood Wrap', '1:00 PM - 2:00 PM', '2023-01-10 08:48:09', 'Done', 200, 'BOYZA THAILAND', 20, 9, 'Stickers', 500),
-(302, 'Julius', 'Irving', '12312312312', '2023-01-11', 'Full Wrap', '2:00 PM - 3:00 PM', '2023-01-10 08:49:13', 'Done', 200, 'ARROW', 20, 9, 'Stickers', 5),
-(303, 'Kyrie', 'Irving', '21312123232', '2023-01-11', '500 - Customized Plate', '3:00 PM - 4:00 PM', '2023-01-10 09:00:30', 'Done', 200, 'ARROW', 20, 8, 'Stickers', 700),
-(304, 'Lebron', 'James', '12312312123', '2023-01-11', '2,000 - Hood Wrap', '4:00 PM - 5:00 PM', '2023-01-10 09:02:16', 'Done', 200, 'ARROW', 20, 7, 'Stickers', 2200),
-(306, 'Klay', 'Thompson', '09789789789', '2023-01-12', '1,000 - HeadLight Film', '7:00 AM - 8:00 AM', '2023-01-10 19:03:34', 'Pending', 200, 'AMERICAN RACING', 2000, 1, 'Cars', 1200),
-(307, 'Draymond', 'Green', '09456465465', '2023-01-12', '1,000 - HeadLight Film', '8:00 AM - 9:00 AM', '2023-01-10 19:04:01', 'Pending', 200, 'CHACKS TO GO', 20, 10, 'Stickers', 1200),
-(308, 'Giannis', 'Antetokounmpo', '12312331231', '2023-01-12', '1,000 - HeadLight Film', '9:00 AM - 10:00 AM', '2023-01-10 19:08:04', 'Pending', 200, '', 0, 0, '', 1200),
-(309, 'Kobe', 'Bryant', '09123123123', '2023-01-12', '1,000 - HeadLight Film', '10:00 AM - 11:00 AM', '2023-01-10 19:10:24', 'Pending', 200, 'aaaaa', 20, 1, 'Stickers', 1200),
-(310, 'Gigi', 'Bryant', '09121231321', '2023-01-12', '2,000 - Hood Wrap', '1:00 PM - 2:00 PM', '2023-01-10 19:10:53', 'Rejected', 200, 'aaaaa', 20, 0, 'Stickers', 2200),
-(311, 'Gigi', 'Bryant', '09123121321', '2023-01-12', '1,000 - HeadLight Film', '1:00 PM - 2:00 PM', '2023-01-10 19:13:03', 'Rejected', 200, 'aaaaa', 20, 0, 'Stickers', 1200);
+INSERT INTO `customer_request` (`id`, `firstName`, `lastName`, `phoneNumber`, `date`, `services`, `time`, `timestamp`, `user_status`, `earnings`, `selectedItem`, `selectedPrice`, `selectedStocks`, `selectedCategory`, `totalAmount`, `refNumber`) VALUES
+(321, 'Lebron', 'James', '09789456123', '2023-01-13', '2,000 - Hood Wrap', '7:00 AM - 8:00 AM', '2023-01-12 03:05:41', 'Pending', 200, 'BOYZA THAILAND', 20, 8, 'Stickers', 2220, 'ARGQHQPC'),
+(322, 'Stephen', 'Curry', '09123456789', '2023-01-13', '2,000 - Hood Wrap', '8:00 AM - 9:00 AM', '2023-01-12 03:06:11', 'Pending', 200, 'No Selected item', 0, 0, '', 2200, 'ARGEMQTJ');
 
 -- --------------------------------------------------------
 
@@ -146,7 +128,7 @@ INSERT INTO `inventory` (`id`, `inventoryImage`, `itemName`, `itemPrice`, `itemS
 (83, '317741456_483057977265464_3928132800673153281_n.jpg', 'JRP', 20, 10, 'Stickers', '2022-12-17 13:25:25'),
 (84, '317814568_388524233463834_3465495384634534929_n.jpg', 'DK RACING', 20, 9, 'Stickers', '2022-12-17 13:25:50'),
 (85, '317831915_1265055284067137_5828192010331717587_n.jpg', 'SKULL', 20, 10, 'Stickers', '2022-12-17 13:26:08'),
-(86, '317854264_632703588647242_4024855050316725167_n.jpg', 'BOYZA THAILAND', 20, 8, 'Stickers', '2022-12-17 20:27:43'),
+(86, '317854264_632703588647242_4024855050316725167_n.jpg', 'BOYZA THAILAND', 20, 7, 'Stickers', '2022-12-17 20:27:43'),
 (87, '317903354_2472732579547230_4305194785556985933_n.jpg', 'HELLO KITTY', 20, 10, 'Stickers', '2022-12-17 13:27:34'),
 (88, '317907573_1275326113322830_8785915066048314661_n.jpg', 'KOBE', 20, 10, 'Stickers', '2022-12-17 13:27:56'),
 (89, '317911576_682655043417020_6291641607554385403_n.jpg', 'CHACKS TO GO', 20, 9, 'Stickers', '2022-12-17 20:39:44');
@@ -242,7 +224,7 @@ ALTER TABLE `inventory_movement`
 -- AUTO_INCREMENT for table `customer_request`
 --
 ALTER TABLE `customer_request`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=321;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=323;
 
 --
 -- AUTO_INCREMENT for table `inventory`
